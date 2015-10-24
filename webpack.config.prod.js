5ca5013365d281fe7170566067ef67d2643d1ae1
Peter Moresi <peter.moresi@gmail.com>
2015-10-24 14:06:26 -0700

    Add prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,10 +25,12 @@ module.exports = {
     })
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
-};
+    loaders: [
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file-loader" },
+      { test: /\.js$/, loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      }]
+      }
+  };
